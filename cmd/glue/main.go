8b98903aaf613e655d551bef4379ff105182288d
Andrea Frittoli <andrea.frittoli@gmail.com>
2019-04-18 10:58:49 +0200

    Working version for devoxx fr demo
    This version provisions the pipeline resources and the two pipelineruns.
    Config values are hardcoded. Target namespace is default since that's
    where the github source and channel exists by default.

--- cmd/glue/main.go
+++ cmd/glue/main.go
@@ -15,8 +15,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-/* This sample is based on the event-display tool
-from knative/eventing-sources */
+/*
+This example is based on the event-display tool from knative/eventing-sources
+*/
 
 package main
 
@@ -27,12 +28,16 @@ import (
 	"fmt"
 	"log"
 	"strings"
+	"time"
 
 	"github.com/cloudevents/sdk-go/pkg/cloudevents"
 	"github.com/cloudevents/sdk-go/pkg/cloudevents/transport/http"
 	"github.com/knative/eventing-sources/pkg/kncloudevents"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	restclient "k8s.io/client-go/rest"
 	tektonv1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1alpha1"
+	tektonv1client "github.com/tektoncd/pipeline/pkg/client/clientset/versioned"
+
 )
 
 /*
@@ -64,21 +69,58 @@ Data,
   {"id":162,"label":""}
 */
 
+/*
+The constants here are a shortcut to config options.
+Eventually it would be nice to have these in a configmap or so.
+*/
+
+const (
+	targetNamespace = "default"
+	targetServiceAccount = "default"
+	imageBaseURL = "us.icr.io/andreaf"
+	testImageName = "health-test-node:latest"
+	apiPipelineName = "dev-test-build-api"
+	fePipelineName = "dev-test-build-frontend"
+)
+
 func process(ctx context.Context, event cloudevents.Event) {
 	// Display the event in any case
-	// display(ctx, event)
-	fmt.Println("Now let's do some extra processing")
+	display(ctx, event)
 
+	// Setup an in-cluster k8s client.
+	// I shouldn't really setup a client for every event... but there it is.
+	cfg, err := restclient.InClusterConfig()
+	if err != nil {
+		log.Fatal("Failed to create a k8s client, ", err)
+	}
+	clientset, err := tektonv1client.NewForConfig(cfg)
+  if err != nil {
+      log.Fatal(err)
+	}
+	// Check if the pipelines are defined
+	pipelines, err := clientset.TektonV1alpha1().Pipelines(targetNamespace).List(metav1.ListOptions{})
+	if err != nil {
+		fmt.Println(err)
+	}
+	pipelinesMap := make(map[string]tektonv1.Pipeline)
+	for _, pipeline := range pipelines.Items {
+		pipelinesMap[pipeline.Name] = pipeline
+	}
+	if _, ok := pipelinesMap[apiPipelineName]; ok == false {
+    log.Fatal("API Pipeline not found")
+	}
+	if _, ok := pipelinesMap[fePipelineName]; ok == false {
+    log.Fatal("FE Pipeline not found")
+	}
 	// Do something if the event is a push
 	switch event.SpecVersion() {
 
 	case cloudevents.CloudEventsVersionV02:
-		fmt.Println("I've got a V2 Cloud event")
-
 		if ec, ok := event.Context.(cloudevents.EventContextV02); ok {
 			switch ec.Type {
 			case "dev.knative.source.github.push":
-				fmt.Println("I've got a push")
+				// Only do something on GitHub push events
+				fmt.Println("Processing a GitHub push")
 
 				// Parse the JSON data
 				var f interface{}
@@ -87,6 +129,8 @@ func process(ctx context.Context, event cloudevents.Event) {
 				    log.Fatal(err)
 				}
 				eventData := f.(map[string]interface{})
+
+				// Extract the repo URL
 				repo, ok := eventData["repository"].(map[string]interface{})
 				if ok == false {
 					log.Fatal("Unexpected data") }
@@ -94,22 +138,214 @@ func process(ctx context.Context, event cloudevents.Event) {
 				if ok == false {
 					log.Fatal("Unexpected data")
 				}
+
+				// Extract the git sha
+				headCommit, ok := eventData["head_commit"].(map[string]interface{})
+				if ok == false {
+					log.Fatal("Unexpected data") }
+				gitSHA, ok := headCommit["id"].(string)
+				if ok == false {
+					log.Fatal("Unexpected data")
+				}
+
+				// Build the image tag
+				var imageTag = gitSHA[0:6]
+
+				// Build the git resource
+				var gitResourceName strings.Builder
+				fmt.Fprintf(&gitResourceName, "git-resource-%s", imageTag)
+
 				gitResource := tektonv1.PipelineResource{
 					ObjectMeta: metav1.ObjectMeta{
-						Name: "git-resource",
-						Namespace: "dev",
+						Name: gitResourceName.String(),
+						Namespace: targetNamespace,
+						Labels: map[string]string{
+							"app": "health",
+							"tag": imageTag,
+						},
 					},
 					Spec: tektonv1.PipelineResourceSpec{
 						Type: tektonv1.PipelineResourceTypeGit,
 						Params: []tektonv1.Param{
-							{
-								Name: repoURL,
-								Value: "https://git-repo",
+							tektonv1.Param {
+								Name: "url",
+								Value: repoURL,
+							},
+							tektonv1.Param {
+								Name: "revision",
+								Value: gitSHA,
 							},
 						},
 					},
 				}
-				fmt.Println(gitResource)
+
+				// Build the API image resource
+				var imageResourceAPIName strings.Builder
+				fmt.Fprintf(&imageResourceAPIName, "image-resource-api-%s", imageTag)
+				var imageAPIURL strings.Builder
+				fmt.Fprintf(&imageAPIURL, "%s/health-api", imageBaseURL)
+
+				imageResourceAPI := tektonv1.PipelineResource{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: imageResourceAPIName.String(),
+						Namespace: targetNamespace,
+						Labels: map[string]string{
+							"app": "health",
+							"tag": imageTag,
+						},
+					},
+					Spec: tektonv1.PipelineResourceSpec{
+						Type: tektonv1.PipelineResourceTypeImage,
+						Params: []tektonv1.Param{
+							tektonv1.Param {
+								Name: "url",
+								Value: imageAPIURL.String(),
+							},
+						},
+					},
+				}
+
+				// Build the Frontend image resource
+				var imageResourceFEName strings.Builder
+				fmt.Fprintf(&imageResourceFEName, "image-resource-frontend-%s", imageTag)
+				var imageFEURL strings.Builder
+				fmt.Fprintf(&imageFEURL, "%s/health-frontend", imageBaseURL)
+
+				imageResourceFE := tektonv1.PipelineResource{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: imageResourceFEName.String(),
+						Namespace: targetNamespace,
+						Labels: map[string]string{
+							"app": "health",
+							"tag": imageTag,
+						},
+					},
+					Spec: tektonv1.PipelineResourceSpec{
+						Type: tektonv1.PipelineResourceTypeImage,
+						Params: []tektonv1.Param{
+							tektonv1.Param {
+								Name: "url",
+								Value: imageFEURL.String(),
+							},
+						},
+					},
+				}
+
+				// Build the API PipelineRun
+				var pipelineRunAPIName strings.Builder
+				fmt.Fprintf(&pipelineRunAPIName, "pipeline-run-api-%s", imageTag)
+
+				pipelineRunAPI := tektonv1.PipelineRun{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: pipelineRunAPIName.String(),
+						Namespace: targetNamespace,
+						Labels: map[string]string{
+							"app": "health",
+							"component": "api",
+							"tag": imageTag,
+						},
+					},
+					Spec: tektonv1.PipelineRunSpec{
+						PipelineRef: tektonv1.PipelineRef{Name: apiPipelineName},
+						Trigger: tektonv1.PipelineTrigger{
+							Type: tektonv1.PipelineTriggerTypeManual},
+						ServiceAccount: targetServiceAccount,
+						Params: []tektonv1.Param{{
+							Name:  "imageTag", Value: imageTag,
+						},},
+						Timeout: &metav1.Duration{Duration: 1 * time.Hour},
+						Resources: []tektonv1.PipelineResourceBinding{
+							{
+								Name: "src",
+								ResourceRef: tektonv1.PipelineResourceRef{
+									Name: gitResourceName.String(),
+								},
+							},
+							{
+								Name: "builtImage",
+								ResourceRef: tektonv1.PipelineResourceRef{
+									Name: imageResourceAPIName.String(),
+								},
+							},
+						},
+					},
+				}
+
+				// Build the FE PipelineRun
+				var pipelineRunFEName strings.Builder
+				fmt.Fprintf(&pipelineRunFEName, "pipeline-run-frontend-%s", imageTag)
+				var testImageNameFull strings.Builder
+				fmt.Fprintf(&testImageNameFull, "%s/%s", imageBaseURL, testImageName)
+
+				pipelineRunFE := tektonv1.PipelineRun{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: pipelineRunFEName.String(),
+						Namespace: targetNamespace,
+						Labels: map[string]string{
+							"app": "health",
+							"component": "frontend",
+							"tag": imageTag,
+						},
+					},
+					Spec: tektonv1.PipelineRunSpec{
+						PipelineRef: tektonv1.PipelineRef{Name: fePipelineName},
+						Trigger: tektonv1.PipelineTrigger{
+							Type: tektonv1.PipelineTriggerTypeManual},
+						ServiceAccount: targetServiceAccount,
+						Params: []tektonv1.Param{
+							{ Name:  "imageTag", Value: imageTag,},
+							{ Name:  "nodeTestImage", Value: testImageNameFull.String(),},
+						},
+						Timeout: &metav1.Duration{Duration: 1 * time.Hour},
+						Resources: []tektonv1.PipelineResourceBinding{
+							{
+								Name: "src",
+								ResourceRef: tektonv1.PipelineResourceRef{
+									Name: gitResourceName.String(),
+								},
+							},
+							{
+								Name: "builtImage",
+								ResourceRef: tektonv1.PipelineResourceRef{
+									Name: imageResourceFEName.String(),
+								},
+							},
+						},
+					},
+				}
+
+				// Provision the resources
+				_, err = clientset.TektonV1alpha1().PipelineResources(
+					targetNamespace).Create(&gitResource)
+				if err != nil {
+				    log.Fatal(err)
+				}
+				_, err = clientset.TektonV1alpha1().PipelineResources(
+					targetNamespace).Create(&imageResourceAPI)
+				if err != nil {
+						log.Fatal(err)
+				}
+				_, err = clientset.TektonV1alpha1().PipelineResources(
+					targetNamespace).Create(&imageResourceFE)
+				if err != nil {
+						log.Fatal(err)
+				}
+
+				// Trigger the pipeline runs
+				_, err = clientset.TektonV1alpha1().PipelineRuns(
+					targetNamespace).Create(&pipelineRunAPI)
+				if err != nil {
+						log.Fatal(err)
+				}
+				_, err = clientset.TektonV1alpha1().PipelineRuns(
+					targetNamespace).Create(&pipelineRunFE)
+				if err != nil {
+						log.Fatal(err)
+				}
+
+				fmt.Println("Provisioned resources and pipelineruns. 😎 Watch with:")
+				fmt.Printf("watch kubectl get all -n %v -l tag=%s\n",
+					targetNamespace, imageTag)
 
 			default:
 				fmt.Println("Ignore non-push event")
